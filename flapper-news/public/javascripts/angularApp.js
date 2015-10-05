31a9ebc4e167b5d2e0cfa246253a8099e0d34cd6
rowmendza <rowmendz@mail.sfsu.edu>
2015-10-05 19:06:09 +0000

    initial commit

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -1,67 +1,122 @@
-var app = angular.module('flapperNews', ['ui.router']);
-app.factory('posts',[function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}]);
+var app = angular.module('flapperNews', ['ui.router'])
+/*config*/
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
 
+    $stateProvider
+        .state('home', {
+  url: '/home',
+  templateUrl: '/home.html',
+  controller: 'MainCtrl',
+  resolve: {
+    postPromise: ['posts', function(posts){
+      return posts.getAll();
+    }]
+  }
+})
+
+        .state('posts', {
+  url: '/posts/{id}',
+  templateUrl: '/posts.html',
+  controller: 'PostsCtrl',
+  resolve: {
+    post: ['$stateParams', 'posts', function($stateParams, posts) {
+      return posts.get($stateParams.id);
+    }]
+  }
+});
+    $urlRouterProvider.otherwise('home');
+}])
+/*Main Ctrl*/
 app.controller('MainCtrl', [
-  '$scope',
-  'posts',
-  function($scope, posts){
+'$scope',
+'posts',
+function($scope, posts){
+    $scope.test = 'Hello world!';
     $scope.posts = posts.posts;
+
     $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-          ]
-          });
-      $scope.title = '';
-      $scope.link = '';
+  if(!$scope.title || $scope.title === '') { return; }
+  posts.create({
+    title: $scope.title,
+    link: $scope.link,
+  });
+  $scope.title = '';
+  $scope.link = '';
+};
+
+    $scope.incrementUpvotes = function(post) {
+        posts.upvote(post);
+    }
+
+}])
+/*Controller*/
+app.controller('PostsCtrl', [
+'$scope',
+'posts',
+'post',
+function($scope, posts, post){
+  $scope.post = post;
+
+    $scope.addComment = function(){
+  if($scope.body === '') { return; }
+  posts.addComment(post._id, {
+    body: $scope.body,
+    author: 'user',
+  }).success(function(comment) {
+    $scope.post.comments.push(comment);
+  });
+        $scope.body = '';
+    };
+  $scope.incrementUpvotes = function(comment){
+  posts.upvoteComment(post, comment);
+};
+}])
+
+app.factory('posts', ['$http', function($http){
+    var o = {
+        posts: []
     };
     
-    $scope.incrementUpvotes = function(post){
-      post.upvotes +=1;
-    };
-  }]);
+    o.get = function(id) {
+  return $http.get('/posts/' + id).then(function(res){
+    return res.data;
+  });
+};
+
+    /* Retrieve posts in post service */
+
+ o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
   
-  app.config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProivder){
-      $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: '/home.html',
-        controller: 'MainCtrl'
-      })
-      .state('posts', {
-        url: '/posts/{id}',
-        templateUrl: '/posts.html',
-        controller: 'PostsCtrl'
-      });
-      $urlRouterProivder.otherwise('home');
-    }]);
-    
-    app.controller('PostsCtrl',[
-      '$scope',
-      '$stateParams',
-      'posts',
-      function($scope, $stateParams, posts){
-        $scope.post = posts.posts[$stateParams.id];
-        $scope.addComment = function(){
-          if($scope.body === '') { return;}
-          $scope.post.comments.push({
-            body: $scope.body,
-            author: 'user',
-            upvotes: 0
-          });
-          $scope.body = '';
-        };
-      }]);
+  o.create = function(post) {
+  return $http.post('/posts', post).success(function(data){
+    o.posts.push(data);
+  });
+};
+
+o.upvote = function(post) {
+  return $http.put('/posts/' + post._id + '/upvote')
+    .success(function(data){
+      post.upvotes += 1;
+    });
+};
+
+o.addComment = function(id, comment) {
+  return $http.post('/posts/' + id + '/comments', comment);
+};
+
+o.upvoteComment = function(post, comment) {
+  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+    .success(function(data){
+      comment.upvotes += 1;
+    });
+};
+
+    return o;
+}]);
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -7,3 +7,94 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+/*GET post */
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+    Post.find(function(err, posts){
+        if(err){ return next(err); }
+        
+        res.json(posts);
+    });
+});
+
+/*POST*/
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
+/*Pre-loading Objects*/
+
+router.param('post', function(req, rest, next,id) {
+  var query = Post.findById(id);
+  
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+    
+    req.post = post;
+    return next();
+  });
+});
+
+/*Return a Single Post */
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
+/*upvote route*/
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+    
+    res.json(post);
+  });
+});
+
+/*Create a comment*/
+router.post('/posts/:post/comments', function(req, res, next) {
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+    
+    comment.save(function(err, comment){
+      if(err){ return next(err); }
+      
+      req.post.comments.push(comment);
+      req.post.save(function(err, post){
+        if(err){ return next(err); }
+        
+        res.json(comment);
+      });
+    });
+});
+
+/*preloading comments*/
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+/*retrieve comments */
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
